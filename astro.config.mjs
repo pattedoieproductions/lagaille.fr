b179e90224a4545d6305bf3685447dbf2c25402c
lpernelle-woosmap <lpernelle@woosmap.com>
2025-12-08 20:16:35 +0100

    chore: Update Astro site URL, base path, and allowed hosts to `pattedoie.pernelle.eu`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lpernelle-woosmap.github.io',
-    base: process.env.NODE_ENV === 'production' ? '/pattedoie' : '/',
+    site: 'http://pattedoie.pernelle.eu',
+    base: '/pattedoie/',
     vite: {
         server: {
-            allowedHosts: ['pattedoie-showcase.loca.lt'],
+            allowedHosts: ['http://pattedoie.pernelle.eu'],
         },
     },
 });
